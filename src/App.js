34a588d95c27263a640f500708875ede4f806edd
Greg <gregoryminezzi@gmail.com>
2022-09-21 20:27:57 +0200

    all logic working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currenciesList, setCurrenciesList] = useState({});
+  const [currencies, setCurrencies] = useState([]);
+  const [result, setResult] = useState(0);
+  const [formData, setFormData] = useState({ input: "", currency: "USD" });
+
+  useEffect(() => {
+    async function getCurrenciesList() {
+      const currenciesList = await fetch(
+        "https://open.er-api.com/v6/latest/USD"
+      );
+      const response = await currenciesList.json();
+      setCurrenciesList(response.rates);
+      setCurrencies(Object.keys(response.rates));
+    }
+    getCurrenciesList();
+  }, []);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setResult(formData.input * currenciesList[formData.currency]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Currency Convertor</h1>
+      <label htmlFor="amount">How much?</label>
+      <input
+        type="text"
+        name="input"
+        value={formData.input}
+        onChange={handleChange}
+      />{" "}
+      <span>USD</span>
+      <br />
+      <label htmlFor="currencies">Choose currency:</label>
+      <select name="currency" value={formData.currency} onChange={handleChange}>
+        {currencies.map((currency) => (
+          <option key={Math.random()}>{currency}</option>
+        ))}
+      </select>
+      <br />
+      <button onClick={handleSubmit}>Convert</button>
+      <h2>
+        <span>
+          {formData.input} USD = &nbsp;
+          {result
+            .toFixed(2)
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          {formData.currency}
+        </span>
+      </h2>
     </div>
   );
 }
